87440369743438ebbeff057f1c24bff72f7a8754
jalvaradoWD <josealvarado1337@gmail.com>
2020-08-28 18:08:51 -0500

    Added Axios and built the Character component

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,19 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Character = () => {
+    const [characterList, setCharacterList]=useState();
+
+    useEffect(()=>{
+    })
+
+    const 
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default Character
